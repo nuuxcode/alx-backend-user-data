060fcb1aad657f3ce9aa3f02a9000a5a8e151b33
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-08 21:48:44 +0100

    😎😎😋 Task6 Implement function to validate if a password matches a hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """doc doc doc"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """doc doc doc"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
