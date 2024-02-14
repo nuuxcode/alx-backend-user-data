b9a6182cfeb0d2ec3a2bcca9bb1aba109141f2ed
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-02-14 03:43:52 +0100

    Task3 3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""doc doc doc """
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """doc doc doc"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """doc doc doc"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """doc doc doc"""
+        return None
